365c9a92e5ed274a8310a337833d2a54fc60e333
MuhammadNafisPutra <muhammadnafisputra@gmail.com>
2026-04-03 23:30:46 +0800

    Add files via upload

---
+++ PRAK1/Soal1.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello World in PHP<br>";
+echo "Nama Praktikan : Muhammad Nafis Putra<br>";
+echo "NIM : 2410817210021";
+
+?>
---
+++ PRAK1/Soal5.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+    <html lang="id">
+        <head>
+         <meta charset="UTF-8">
+            <title>PRAK104</title>
+        <style>
+        table {
+            border: 1px solid black;
+            border-spacing: 2px; 
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 4px 8px; 
+        }
+        th {
+            background-color: #ff0000;
+            font-size: 24px; 
+            padding: 20px; 
+        }
+        </style>
+        </head>
+    <body>
+
+<?php
+function Smartphones() {
+    $merkSmartphone = [
+        "Samsung Galaxy S22",
+        "Samsung Galaxy S22+",
+        "Samsung Galaxy A03",
+        "Samsung Galaxy Xcover 5"
+    ];
+
+    echo "<table>";
+    echo "<tr><th>Daftar Smartphone Samsung</th></tr>";
+    
+    foreach ($merkSmartphone as $phone) {
+        echo "<tr><td>$phone</td></tr>";
+    }
+    
+    echo "</table>";
+}
+
+Smartphones();
+?>
+
+    </body>
+</html>
